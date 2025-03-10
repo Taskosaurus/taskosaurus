d9d809e4b19428a40b3cb41066cb5c651b235908
Isabella Baumann <i.baumann@students.htl-leonding.ac.at>
2025-03-10 16:15:49 +0100

    template swift project

---
+++ frontend/taskosaurus/taskosaurusApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskosaurusApp.swift
+//  taskosaurus
+//
+//  Created by Baumann Isabella on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct taskosaurusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
