6fbb5432f12050c0baf38f01fbd67658f8976971
Baumann Isabella <it210181@WS8A02.htl-leonding.ac.at>
2025-03-12 11:16:30 +0100

    QR Scanner Demo added

---
+++ frontend/QRScannerDemo/QRScannerDemo/QRScannerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerDemoApp.swift
+//  QRScannerDemo
+//
+//  Created by Grad Kinga on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
