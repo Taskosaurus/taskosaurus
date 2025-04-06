c08ce488d0d6b5f1f9003a87c64aaa9095bdbe6c
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-04-06 20:11:43 +0200

    changed answers from api of a question to collected

---
+++ backend/src/main/java/at/htlleonding/dto/GroupQuestionAnswerCollectedDto.java
@@ -0,0 +1,4 @@
+package at.htlleonding.dto;
+
+public record GroupQuestionAnswerCollectedDto(Long answeringId, Long answeredId, String answeredName, Long count) {
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,6 +4,3 @@ INSERT INTO Question (id, question) VALUES (2, 'Wie viele Planeten gibt es in un
 INSERT INTO Question (id, question) VALUES (3, 'Wer hat die Relativitätstheorie formuliert?');
 INSERT INTO Question (id, question) VALUES (4, 'Was ist die Wurzel von 64?');
 INSERT INTO Question (id, question) VALUES (5, 'Welches ist das größte Säugetier der Welt?');
-
-
-INSERT INTO GroupEntity (id, name) VALUES (1, 'Testgruppe');
