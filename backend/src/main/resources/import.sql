f980e77395f0456ebde14191c211c2deb0ca9f61
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-04-07 11:02:14 +0200

    Added better questions and moved the frontend .gitignore file into the frontend folder

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,22 @@
 -- Fragen einfügen
-INSERT INTO Question (id, question) VALUES (1, 'Was ist die Hauptstadt von Frankreich?');
-INSERT INTO Question (id, question) VALUES (2, 'Wie viele Planeten gibt es in unserem Sonnensystem?');
-INSERT INTO Question (id, question) VALUES (3, 'Wer hat die Relativitätstheorie formuliert?');
-INSERT INTO Question (id, question) VALUES (4, 'Was ist die Wurzel von 64?');
-INSERT INTO Question (id, question) VALUES (5, 'Welches ist das größte Säugetier der Welt?');
+INSERT INTO Question (question) VALUES ('Wer würde am ehesten versuchen in einer Fremdsprache zu kommunizieren, indem er einfach deutsche Wörter mit einem Akzent ausspricht?');
+INSERT INTO Question (question) VALUES ('Wer von uns würde am wenigsten das Haus verlassen, wenn der Handyakku fast leer ist, und deshalb lieber zu spät kommen?');
+INSERT INTO Question (question) VALUES ('Wer lebt am stärksten nach dem Motto "Man lebt nur einmal im Leben"?');
+INSERT INTO Question (question) VALUES ('Wer würde eine gesamte Gruppenreise dokumentieren, aber die Fotos nie mit uns teilen?');
+INSERT INTO Question (question) VALUES ('Wer überreagiert am schnellsten?');
+INSERT INTO Question (question) VALUES ('Wer ist am schnellsten Teil von enuen Trends?');
+INSERT INTO Question (question) VALUES ('Jeder in der Gruppe zeigt dir einen Song. Wer hat den unvorhersehbarsten Musikgeschmack?');
+INSERT INTO Question (question) VALUES ('Wer würde es am kürzesten in einer dreckigen Wohnung aushalten? Putzen ist nicht erlaubt!');
+INSERT INTO Question (question) VALUES ('Wer ist am schnellsten Teil von enuen Trends?');
+INSERT INTO Question (question) VALUES ('Wer würde am ehesten kriminell werden, um einem Freund bei etwas zu helfen?');
+INSERT INTO Question (question) VALUES ('Wer würde am ehesten versuchen, sich mit Wortwitz und Charme aus einem Strafzettel herauszureden?');
+INSERT INTO Question (question) VALUES ('Wer könnte die beste TikTok-Tanzperformance hinlegen?');
+INSERT INTO Question (question) VALUES ('Wen würde man öffentlich am wenigsten als "politisch korrekt" einstufen?');
+INSERT INTO Question (question) VALUES ('Ganz alleine in einem leeren Raum: Wer würde am schnellsten die Nerven verlieren?');
+
+
+
+
+
+
+
