10608ae659dd41c8f3cda3dca214b2f717c7196d
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-04-21 19:09:20 +0200

    added test data imports to backend

--- backend/src/main/java/at/htlleonding/repository/QuestionRepository.java
+++ backend/src/main/java/at/htlleonding/repository/QuestionRepository.java
@@ -81,7 +81,7 @@ public class QuestionRepository {
         GroupQuestion question = getGroupQuestionForDate(date, group);
         return entityManager.createQuery("SELECT new at.htlleonding.dto.GroupQuestionAnswerCollectedDto(" +
                         "gqa.answeringPlayer.id, gqa.answer.id, gqa.answer.name, count(gqa.answer)) " +
-                        "FROM GroupQuestionAnswer gqa WHERE gqa.groupQuestion = :question GROUP BY gqa.answer",
+                        "FROM GroupQuestionAnswer gqa WHERE gqa.groupQuestion = :question GROUP BY gqa.answer, gqa.answeringPlayer",
                         GroupQuestionAnswerCollectedDto.class).setParameter("question", question).getResultList();
     }
 
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
--- Fragen einfügen
+-- Add Questions
 INSERT INTO Question (question) VALUES ('Wer würde am ehesten versuchen in einer Fremdsprache zu kommunizieren, indem er einfach deutsche Wörter mit einem Akzent ausspricht?');
 INSERT INTO Question (question) VALUES ('Wer von uns würde am wenigsten das Haus verlassen, wenn der Handyakku fast leer ist, und deshalb lieber zu spät kommen?');
 INSERT INTO Question (question) VALUES ('Wer lebt am stärksten nach dem Motto "Man lebt nur einmal im Leben"?');
@@ -15,8 +15,36 @@ INSERT INTO Question (question) VALUES ('Wen würde man öffentlich am wenigsten
 INSERT INTO Question (question) VALUES ('Ganz alleine in einem leeren Raum: Wer würde am schnellsten die Nerven verlieren?');
 
 
+-- test data
+INSERT INTO GroupEntity (name, id) VALUES ('Familie', 1);
+INSERT INTO GroupEntity (name, id) VALUES ('4AHITM', 2);
+
+-- Group 1 (Familie)
+INSERT INTO Player (name, group_id, id) VALUES('Maria', 1, 1);
+INSERT INTO Player (name, group_id, id) VALUES('Max', 1, 2);
+INSERT INTO Player (name, group_id, id) VALUES('Herbert', 1, 3);
+INSERT INTO Player (name, group_id, id) VALUES('Frederike', 1, 4);
+INSERT INTO Player (name, group_id, id) VALUES('Gertrude', 1, 5);
+
+-- Group 2 (4AHITM)
+INSERT INTO Player (name, group_id, id) VALUES('Isabella', 2, 6);
+INSERT INTO Player (name, group_id, id) VALUES('Thomas', 2, 7);
+INSERT INTO Player (name, group_id, id) VALUES('Kinga', 2, 8);
+INSERT INTO Player (name, group_id, id) VALUES('Timon', 2, 9);
+INSERT INTO Player (name, group_id, id) VALUES('Lien', 2, 10);
+INSERT INTO Player (name, group_id, id) VALUES('Stefanie', 2, 11);
+INSERT INTO Player (name, group_id, id) VALUES('Christof', 2, 12);
+INSERT INTO Player (name, group_id, id) VALUES('Tobi', 2, 13);
 
 
+-- Insert a GroupQuestion using today's date
+INSERT INTO GroupQuestion (question_id, group_id, date, id)
+VALUES (7, 1, CURRENT_DATE, 1);
 
-
-
+INSERT INTO GroupQuestionAnswer (answering_player_id, answer, group_question_id, id)
+VALUES
+    (1, 2, 1, 1),
+    (2, 4, 1, 2),
+    (3, 5, 1, 3),
+    (4, 2, 1, 4),
+    (5, 4, 1, 5);
