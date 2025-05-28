ee2f918fc726c1de0a186e655b2ae35ccb50868b
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-05-28 12:13:18 +0200

    changed something

--- backend/src/main/java/at/htlleonding/dto/FetchedGroupDto.java
+++ backend/src/main/java/at/htlleonding/dto/FetchedGroupDto.java
@@ -4,5 +4,5 @@ import at.htlleonding.model.Player;
 
 import java.util.List;
 
-public record FetchedGroupDto(Long id, String name, String link, List<Player> players, Integer count, boolean answered) {
+public record FetchedGroupDto(Long id, String name, String link, List<Player> players, Long count, boolean answered) {
 }
