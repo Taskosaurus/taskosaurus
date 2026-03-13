aabe59f5551a84e634caa3a586f4eaae66d5c861
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2026-03-13 14:21:59 +0100

    hashes updated and version number

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -18,25 +18,24 @@ INSERT INTO GroupEntity (name, link) VALUES ('Die 5 Freunde', 'http://192.168.13
 INSERT INTO GroupEntity (name, link) VALUES ('Die 3 ???', 'http://192.168.137.135:8080/api/group/join/4');
 
 
-INSERT INTO Player (name, password) VALUES('Isabella', '$2a$12$0vV1vR9HjX6h6.q4T/R.Ue6rVnU6X1vI9S8L7M6K5J4I3H2G1F0E.');
-INSERT INTO Player (name, password) VALUES('Max',      '$2a$12$K1G2H3I4J5K6L7M8N9O0P.u8u7v6w5x4y3z2A1B2C3D4E5F6G7H8I');
-INSERT INTO Player (name, password) VALUES('Herbert',  '$2a$12$mP5R7S9T1U3V5W7X9Y1Z2.uYvXwWvUuTtSsRrQqPpOoNnMmLlKkJj');
-INSERT INTO Player (name, password) VALUES('Frederike','$2a$12$fL7mN8o9p0q1r2s3t4u5v.uY7x6w5v4u3t2s1r0q9p8o7n6m5l4k');
-INSERT INTO Player (name, password) VALUES('Gertrude', '$2a$12$wR3tV5zX8b7p1f4h7j0m4.6O9l8k7j6i5h4g3f2e1d0c9b8a7y6x');
-INSERT INTO Player (name, password) VALUES('Thomas',   '$2a$12$G1J2K3L4M5N6O7P8Q9R0S.uS8r7q6p5o4n3m2l1k0j9i8h7g6f5e');
-INSERT INTO Player (name, password) VALUES('Kinga',    '$2a$12$T2uV3wX4y5z6A7B8C9D0E.uT5s4r3q2p1o0n9m8l7k6j5i4h3g2f');
-INSERT INTO Player (name, password) VALUES('Timon',    '$2a$12$M1N2O3P4Q5R6S7T8U9V0W.uR2q1p0o9n8m7l6k5j4i3h2g1f0e9d');
-INSERT INTO Player (name, password) VALUES('Lien',     '$2a$12$B1C2D3E4F5G6H7I8J9K0L.uL9k8j7i6h5g4f3e2d1c0b9a8z7y6');
-INSERT INTO Player (name, password) VALUES('Stefanie', '$2a$12$X1Y2Z3A4B5C6D7E8F9G0H.uH6g5f4e3d2c1b0a9z8y7x6w5v4u3');
-INSERT INTO Player (name, password) VALUES('Christoph','$2a$12$O1P2Q3R4S5T6U7V8W9X0Y.uY7x6w5v4u3t2s1r0q9p8o7n6m5l4k');
-INSERT INTO Player (name, password) VALUES('Tobi',     '$2a$12$Z1A2B3C4D5E6F7G8H9I0J.uJ0i9h8g7f6e5d4c3b2a1z0y9x8w7v');
-INSERT INTO Player (name, password) VALUES('Timmy',    '$2a$12$K1L2M3N4O5P6Q7R8S9T0U.uU1t0s9r8q7p6o5n4m3l2k1j0i9h8g');
-INSERT INTO Player (name, password) VALUES('Georgina', '$2a$12$V1W2X3Y4Z5A6B7C8D9E0F.uF9e8d7c6b5a4z3y2x1w0v9u8t7s6');
-INSERT INTO Player (name, password) VALUES('Julian',   '$2a$12$G1H2I3J4K5L6M7N8O9P0Q.uQ0p9o8n7m6l5k4j3i2h1g0f9e8d7c');
-INSERT INTO Player (name, password) VALUES('Anne',     '$2a$12$R1S2T3U4V5W6X7Y8Z9A0B.uB0a9z8y7x6w5v4u3t2s1r0q9p8o7n');
-INSERT INTO Player (name, password) VALUES('Peter',    '$2a$12$C1D2E3F4G5H6I7J8K9L0M.uM0l9k8j7i6h5g4f3e2d1c0b9a8z7y');
-INSERT INTO Player (name, password) VALUES('Bob',      '$2a$12$N1O2P3Q4R5S6T7U8V9W0X.uX0w9v8u7t6s5r4q3p2o1n0m9l8k7j');
-
+INSERT INTO Player (name, password) VALUES('Isabella', '$2a$12$R.O7pI9mDkF5hA6G7H8I9OeL1R2S3T4U5V6W7X8Y9Z0A1B2C3D4E5');
+INSERT INTO Player (name, password) VALUES('Max',      '$2a$12$S.O8pJ9nElG6iB7H8I9J0OfM2S3T4U5V6W7X8Y9Z0A1B2C3D4E5F6');
+INSERT INTO Player (name, password) VALUES('Herbert',  '$2a$12$T.P9qK0oFmH7jC8I9J0K1PgN3T4U5V6W7X8Y9Z0A1B2C3D4E5F6G7');
+INSERT INTO Player (name, password) VALUES('Frederike','$2a$12$U.Q0rL1pGnI8kD9J0K1L2QhO4U5V6W7X8Y9Z0A1B2C3D4E5F6G7H');
+INSERT INTO Player (name, password) VALUES('Gertrude', '$2a$12$V.R1sM2qHoJ9lE0K1L2M3RiP5V6W7X8Y9Z0A1B2C3D4E5F6G7H8');
+INSERT INTO Player (name, password) VALUES('Thomas',   '$2a$12$W.S2tN3rIpK0mF1L2M3N4SjQ6W7X8Y9Z0A1B2C3D4E5F6G7H8I9');
+INSERT INTO Player (name, password) VALUES('Kinga',    '$2a$12$X.T3uO4sJqL1nG2M3N4O5TkR7X8Y9Z0A1B2C3D4E5F6G7H8I9J0');
+INSERT INTO Player (name, password) VALUES('Timon',    '$2a$12$Y.U4vP5tKrM2oH3N4O5P6UlS8Y9Z0A1B2C3D4E5F6G7H8I9J0K1');
+INSERT INTO Player (name, password) VALUES('Lien',     '$2a$12$Z.V5wQ6uLsN3pI4O5P6Q7VmT9Z0A1B2C3D4E5F6G7H8I9J0K1L2');
+INSERT INTO Player (name, password) VALUES('Stefanie', '$2a$12$a.W6xR7vMtO4qJ5P6Q7R8WnU0A1B2C3D4E5F6G7H8I9J0K1L2M3');
+INSERT INTO Player (name, password) VALUES('Christoph','$2a$12$b.X7yS8wNuP5rK6Q7R8S9XoV1B2C3D4E5F6G7H8I9J0K1L2M3N4');
+INSERT INTO Player (name, password) VALUES('Tobi',     '$2a$12$c.Y8zT9xOvQ6sL7R8S9T0YpW2C3D4E5F6G7H8I9J0K1L2M3N4O5');
+INSERT INTO Player (name, password) VALUES('Timmy',    '$2a$12$d.Z9aU0yPwR7tM8S9T0U1ZqX3D4E5F6G7H8I9J0K1L2M3N4O5P6');
+INSERT INTO Player (name, password) VALUES('Georgina', '$2a$12$e.A0bV1zQxS8uN9T0U1V2ArY4E5F6G7H8I9J0K1L2M3N4O5P6Q');
+INSERT INTO Player (name, password) VALUES('Julian',   '$2a$12$f.B1cW2aRyT9vO0U1V2W3BsZ5F6G7H8I9J0K1L2M3N4O5P6Q7');
+INSERT INTO Player (name, password) VALUES('Anne',     '$2a$12$g.C2dX3bSzU0wP1V2W3X4CtA6G7H8I9J0K1L2M3N4O5P6Q7R8');
+INSERT INTO Player (name, password) VALUES('Peter',    '$2a$12$h.D3eY4cT0V1xQ2W3X4Y5DuB7H8I9J0K1L2M3N4O5P6Q7R8S9');
+INSERT INTO Player (name, password) VALUES('Bob',      '$2a$12$i.E4fZ5dU1W2yR3X4Y5Z6EvC8I9J0K1L2M3N4O5P6Q7R8S9T0');
 
 INSERT INTO Player_Group (player_id, group_id) VALUES (1, 1);
 INSERT INTO Player_Group (player_id, group_id) VALUES (2, 1);
--- frontend/Android/app/build.gradle.kts
+++ frontend/Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "at.htlleonding.taskosaurus"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
